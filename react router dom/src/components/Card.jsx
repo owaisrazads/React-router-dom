ef601ffbd876e3c49555d146d37b84cc9b4a9b92
unknown <oraza7969@gmail.com>
2024-01-09 03:20:54 -0800

    react router dom

--- react router dom/src/components/Card.jsx
+++ react router dom/src/components/Card.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Card = ({src, title, price, description}) => {
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-    <figure><img src={src} className='w-[180px] mt-5' alt="Shoes" /></figure>
+ 
+      <div className="card card-compact w-96 bg-base-100 mt-5 shadow-xl">
+    <figure><img src={src} className='w-[180px] mt-8' alt="Shoes" /></figure>
     <div className="card-body">
       <h2 className="card-title">{title}</h2>
       <p>{description}</p>
@@ -13,6 +14,7 @@ const Card = ({src, title, price, description}) => {
       </div>
     </div>
   </div>
+ 
   )
 }
 
